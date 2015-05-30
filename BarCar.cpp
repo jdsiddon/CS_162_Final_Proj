cefeae89edb0343500ceb792375333b910878e72
jsdiddon <justin@delaris.com>
2015-05-30 12:40:24 -0700

    Added BarCar.cpp to repo.

---
+++ BarCar.cpp
@@ -0,0 +1,40 @@
+#include <iostream> // Debugging.
+
+#include "Room.hpp"
+#include "BarCar.hpp"
+
+
+/*********************************************************************
+ ** Function: BlueMen
+ ** Description: Default BlueMen constructor. Calls Character class
+ ** default constructor.
+ ** Parameters: None.
+ ** Pre-Conditions: No BlueMen created.
+ ** Post-Conditions: BlueMen created.
+ *********************************************************************/
+BarCar::BarCar()
+  : Room() {
+}
+
+/*********************************************************************
+ ** Function: BlueMen
+ ** Description: Default BlueMen destructor.
+ ** Parameters: None.
+ ** Pre-Conditions: BlueMen.
+ ** Post-Conditions: BlueMen object destroyed.
+ *********************************************************************/
+BarCar::~BarCar() {
+
+}
+
+
+/*********************************************************************
+ ** Function: chatWithBarTender
+ ** Description: Default BlueMen destructor.
+ ** Parameters: None.
+ ** Pre-Conditions: BlueMen.
+ ** Post-Conditions: BlueMen object destroyed.
+ *********************************************************************/
+void BarCar::chatWithBarTender() {
+
+}
