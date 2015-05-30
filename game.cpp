dfff69e3fe6f959f887cf01799ab534e4262eb29
jsdiddon <justin@delaris.com>
2015-05-30 15:22:56 -0700

    Create 'createTrain' method in game file to initialize the train area.

--- game.cpp
+++ game.cpp
@@ -7,6 +7,10 @@
 #include "BarCar.hpp"
 #include "Queue.hpp"
 
+/* Prototypes */
+Queue* createTrain();
+
+
 int main() {
   std::string tempName;
 
@@ -16,26 +20,14 @@ int main() {
   Player *player1 = new Player(tempName); /* Create player. */
   std::cout << player1->getName() << std::endl;
 
-  /* Create test room */
-  int barCarNum = 1;
-  int leftCarNum = 2;
-  int rightCarNum = 3;
-  Room *barCar = new BarCar(barCarNum);
-  Room *leftCar = new BarCar(leftCarNum);
-  Room *rightCar = new BarCar(rightCarNum);
+  createTrain();  /* Create the train. */
 
   /* DEBUGGING CODE */
   // std::cout << barCar->getRoomType() << std::endl; /* Get room name */
   /* END DEBUGGING CODE */
 
 
-  Queue *train = new Queue();
-  /* Add car to train */
-  train->add(barCar, leftCar, rightCar);
 
-  Room *currentCar = train->getCurrentNode();
-
-  std::cout << currentCar->getRoomType();
 
 
   /* Free memory */
@@ -43,4 +35,25 @@ int main() {
   player1 = 0;
 
   return 0;
+}
+
+Queue* createTrain() {
+  /* DEBUGGING CODE, create test room with outside spaces */
+  int barCarNum = 1;
+  int leftCarNum = 2;
+  int rightCarNum = 3;
+
+  Room *barCar = new BarCar(barCarNum);
+  Room *leftCar = new BarCar(leftCarNum);
+  Room *rightCar = new BarCar(rightCarNum);
+
+  Queue *train = new Queue();
+
+  for (int i = 0; i < 4; i++) {
+    /* Add car to train */
+    train->add(barCar, leftCar, rightCar);
+    std::cout << std::endl;
+  }
+  /* END DEBUGGING CODE, create test room with outside spaces */
+  return train;
 }
